4b110f3778b6becebb27b2d642529ca6ba31d749
Ana-Dare <analauradare467@gmail.com>
2025-04-14 22:23:32 -0300

    Add files via upload

---
+++ Troca Cor_files/ap.js
@@ -0,0 +1,39 @@
+
+function clique(){
+
+	document.getElementById("fundo").style.backgroundColor="#000";
+	
+	}
+
+function troca(cor){
+	if (cor == "vermelho"){
+		document.getElementById("quadrado").style.backgroundColor="red";
+	}
+
+	else if(cor == "verde"){
+			document.getElementById("quadrado").style.backgroundColor="green";
+	}
+
+	else if(cor == "amarelo"){
+		document.getElementById("quadrado").style.backgroundColor="#e3e030";
+}
+
+else if(cor == "rosa"){
+	document.getElementById("quadrado").style.backgroundColor="#e048e8";
+}
+
+else if(cor == "azul_claro"){
+	document.getElementById("quadrado").style.backgroundColor="#99c4d1";
+}
+
+else if(cor == "marrom"){
+	document.getElementById("quadrado").style.backgroundColor="#5c422d";
+}
+
+	else{
+			document.getElementById("quadrado").style.backgroundColor="#525252";
+	};
+	
+	};
+
+
